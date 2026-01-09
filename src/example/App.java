0e5c91469a1d8c9ee49d23598cca9fe53be06723
t2025-m0040 <e0321e@nate.com>
2026-01-09 11:55:56 +0900

    두 개의 양의 정수 입력 기능 구현

---
+++ src/example/App.java
@@ -0,0 +1,15 @@
+package example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = scanner.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = scanner.nextInt();
+
+    }
+}
